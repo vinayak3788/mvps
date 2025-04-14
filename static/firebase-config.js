9431a28028461fd19aa7199b8a5bff076aeffadb
vinayak3788 <vinayak3788@gmail.com>
2025-04-14 23:54:39 +0530

    Update firebase-config.js

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -1,11 +1,1 @@
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAqX2UKQgFuy0AAlDXwzrZGvwFgEsQ5ePQ",
-  authDomain: "mvps-7adb0.firebaseapp.com",
-  projectId: "mvps-7adb0",
-  storageBucket: "mvps-7adb0.firebasestorage.app",
-  messagingSenderId: "617672630606",
-  appId: "1:617672630606:web:12cf63643278fdb2677e1f",  
-};
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+\\
